8a1bb850d3e3792b59299172df891626c8574ccb
iubns <iubns@naver.com>
2025-05-04 11:25:05 +0900

    update: 자동으로 턴을 바꿔가며 학습 하도록 수정

--- learn.py
+++ learn.py
@@ -5,18 +5,21 @@ from sb3_contrib.common.wrappers import ActionMasker
 from MiniChessEnv import MiniChessEnv
 
 # 학습할 모델의 턴
-user = 'up'  # up or down
+user = 'down'  # up or down
+for i in range(1, 100):
+    print("Training model_" + str(i))
+    user = 'up' if i % 2 == 0 else 'down'
 
-# 상대 모델의 턴 설정
-enemy = 'down' if user == 'up' else 'up'
+    # 상대 모델의 턴 설정
+    enemy = 'down' if user == 'up' else 'up'
 
-# 환경 및 모델 생성
-env = gym.make("MiniChess-v0")
-env = ActionMasker(env, lambda env: env.unwrapped.get_valid_actions())
-embedded_model = MaskablePPO.load("./model_" + str(enemy), env=env)
-env.unwrapped.set_embedded_env(env, embedded_model, user = 0 if user == 'up' else 1)
-model = MaskablePPO("MultiInputPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
-model.save("./model_" + str(user))
+    # 환경 및 모델 생성
+    env = gym.make("MiniChess-v0")
+    env = ActionMasker(env, lambda env: env.unwrapped.get_valid_actions())
+    embedded_model = MaskablePPO.load("./model_" + str(enemy), env=env)
+    env.unwrapped.set_embedded_env(env, embedded_model, user = 0 if user == 'up' else 1)
+    model = MaskablePPO("MultiInputPolicy", env, verbose=1)
+    model.learn(total_timesteps=100_000)
+    model.save("./model_" + str(user))
 
-print("model_" + str(user) + " saved")
+    print("model_" + str(user) + " saved")
